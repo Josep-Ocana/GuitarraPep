c3cc67dfdee9cc0eba672dfb941ccf0bb685d391
Pepekid <j.ocanapuigdevall@gmail.com>
2024-10-03 11:40:23 +0200

    add props in App and guitar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import Footer from './components/Footer';
 import Guitar from './components/Guitar';
 import Header from './components/Header';
-
+import { db } from './data/db';
 function App() {
+  const [data, setData] = useState(db);
+
   return (
     <>
       <Header />
@@ -10,14 +13,9 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} />
+          ))}
         </div>
       </main>
 

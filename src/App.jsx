0376acdab38173cd41e7bd9798203d865f836063
Pepekid <j.ocanapuigdevall@gmail.com>
2024-10-04 12:50:39 +0200

    add dinamic cart and total price

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,39 @@ import Footer from './components/Footer';
 import Guitar from './components/Guitar';
 import Header from './components/Header';
 import { db } from './data/db';
+
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+
+    if (itemExist !== -1) {
+      // Existe en el carrito
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
 
   return (
     <>
-      <Header />
+      <Header cart={cart} />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </main>

dbef17414780b1036be7f4faae97f36034275f02
Pepekid <j.ocanapuigdevall@gmail.com>
2024-10-04 13:18:25 +0200

    add useMemo in Header functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { useMemo } from 'react';
+
 export default function Header({ cart }) {
   // State Derivado
-  const isEmpty = () => cart.length === 0;
-  const cartTotal = () =>
-    cart.reduce((total, item) => total + item.quantity * item.price, 0);
-
+  const isEmpty = useMemo(() => cart.length === 0, [cart]);
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
+    [cart]
+  );
   return (
     <header className="py-5 header">
       <div className="container-xl">
@@ -26,7 +29,7 @@ export default function Header({ cart }) {
               />
 
               <div id="carrito" className="bg-white p-3">
-                {isEmpty() ? (
+                {isEmpty ? (
                   <p className="text-center">El carrito esta vacio</p>
                 ) : (
                   <>
@@ -72,7 +75,7 @@ export default function Header({ cart }) {
                     </table>
                     <p className="text-end">
                       Total pagar:
-                      <span className="fw-bold">{cartTotal()} €</span>
+                      <span className="fw-bold">{cartTotal} €</span>
                     </p>
                   </>
                 )}
